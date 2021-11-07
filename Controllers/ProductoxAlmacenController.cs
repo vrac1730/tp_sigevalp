b0869edeb48c875087a11f262eb7503fb650e2ef
victor rufino alarcon campos <virualca@hotmail.com>
2021-11-07 14:45:38 -0500

    Generar Reporte & Filtros

--- Controllers/ProductoxAlmacenController.cs
+++ Controllers/ProductoxAlmacenController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using SIGEVALP.Models;
 using System.Data.Entity;
+using Rotativa;
 
 namespace SIGEVALP.Controllers
 {
@@ -23,12 +24,12 @@ namespace SIGEVALP.Controllers
         }
 
         // GET: ProductoxAlmacen
-        public ActionResult Index(string cadena, string cod)
+        public ActionResult Index(string cad, string cod)
         {
             var result = db.ProductosxAlmacen.Include(p => p.Producto.Categoria);
             //validar simbolos y caracteres no permitidos
             //mostrar otra vista para resultados no encontrados?
-            if (String.IsNullOrWhiteSpace(cadena) && String.IsNullOrWhiteSpace(cod))
+            if (String.IsNullOrWhiteSpace(cad) && String.IsNullOrWhiteSpace(cod))
             {
                 return View(result.ToList());
             }
@@ -39,13 +40,41 @@ namespace SIGEVALP.Controllers
                 return View(r1);
             }
 
-            else if (cadena.Length > 0)
+            else if (cad.Length > 0)
             {
-                var r2 = result.Where(p => p.Producto.nombre.Contains(cadena)).ToList();
+                var r2 = result.Where(p => p.Producto.nombre.Contains(cad)).ToList();
                 return View(r2);
             }
 
             return View(result.ToList());
+        }      
+
+        public ActionResult Report(string codigo, string cadena)
+        {
+            var result = db.ProductosxAlmacen.Include(p => p.Producto.Categoria);
+
+            if (String.IsNullOrWhiteSpace(cadena) && String.IsNullOrWhiteSpace(codigo))
+            {
+                return View(result.ToList());
+            }
+
+            if (cadena.Length > 0)
+            {
+                var r2 = result.Where(p => p.Producto.nombre.Contains(cadena)).ToList();
+                return View(r2);
+            }
+            if (codigo.Length > 0)
+            {
+                var r1 = result.Where(p => p.Producto.codigo == codigo).ToList();
+                return View(r1);
+            }
+
+            return View(result.ToList());
+        }
+
+        public ActionResult Print()
+        {
+            return new ActionAsPdf("Report") { FileName = "Reporte_Stock.pdf" };
         }
     }
 }
