1ec4f4c8a0aa1bd258f81856dc95e3ccc0a74ef7
victor rufino alarcon campos <virualca@hotmail.com>
2021-11-08 21:26:05 -0500

    Validar detalles, excepto símbolos

---
+++ Scripts/master-detail_validation.js
@@ -0,0 +1,18 @@
+﻿function EqualsZero(c) {
+    return c == 0 ? "Seleccione un producto" : "";
+}
+function LessThanZero(c) {
+    return c < 0 ? "Este campo no puede ser negativo" : "";
+}
+function IsEmpty(c) {
+    return c == "" ? "Este campo es requerido" : "";
+}
+function EmptyOrMinor(c) {
+    return c == "" ? IsEmpty(c) : LessThanZero(c);
+}
+function IsSymbol(c) {
+} 
+function EmptyOrMinorOrSymbol(c) {
+    return c == "" ? IsEmpty(c) : c < 0 ? LessThanZero(c) : IsSymbol(c);
+}
+export { EqualsZero, IsEmpty, LessThanZero, EmptyOrMinor };
