2c1f556b97fc2c2a5e47debd9d03cd342ff5f0c3
victor rufino alarcon campos <virualca@hotmail.com>
2021-12-07 22:02:27 -0500

    import as detail

--- Scripts/master-detail_validation.js
+++ Scripts/master-detail_validation.js
@@ -1,21 +1,44 @@
-﻿function EqualsZero(c) {
+﻿export function EqualsZero(c) {
     return c == 0 ? "Seleccione un producto" : "";
 }
-function LessThanZero(c) {
+export function LessThanZero(c) {
     return c < 0 ? "Este campo no puede ser negativo" : "";
 }
-function IsEmpty(c) {
+export function IsEmpty(c) {
     return c == "" ? "Este campo es requerido" : "";
 }
-function EmptyOrMinor(c) {
+export function EmptyOrMinor(c) {
     return c == "" ? IsEmpty(c) : LessThanZero(c);
 }
-function IsSymbol(c) {
+export function IsSymbol(c) {
 } 
-function EmptyOrMinorOrSymbol(c) {
+export function EmptyOrMinorOrSymbol(c) {
     return c == "" ? IsEmpty(c) : c < 0 ? LessThanZero(c) : IsSymbol(c);
 }
-function GetId(c) {
+export function GetId(c) {
     return c.substr(4, c.length);
 }
-export { EqualsZero, IsEmpty, LessThanZero, EmptyOrMinor, GetId };
+export function createButton(name, tdopccion, funct) {//[name, funct]
+    let button = document.createElement("input");
+    button.value = name;
+    button.type = "button";
+    button.onclick = funct;
+    button.className = "btn btn-default";
+    tdopccion.appendChild(button);
+}
+export function createTd(value, tr) {//[value]
+    let td = document.createElement("td");
+    td.innerHTML = value;
+    tr.appendChild(td);
+}
+export function createInput(id, model, prop, value, div) {//[prop, value]
+    let input = document.createElement("input");
+    createInputId(input, id, model, prop);
+    input.value = value;
+    input.type = "hidden";
+    div.appendChild(input);
+}
+export function createInputId(input, id, model, prop) {//[input] [prop] misma longitud
+    input.id = model + "[" + id + "]." + prop;
+    input.name = input.id;
+}
